451f253bff88d2bd991e187841f890f737066e3c
sebnozzi <sen@192.168.0.12>
2013-05-28 21:03:04 +0200

    Showing image

--- src/main/scala/com/sebnozzi/slidingpuzzle/SlidingPuzzle.scala
+++ src/main/scala/com/sebnozzi/slidingpuzzle/SlidingPuzzle.scala
@@ -5,6 +5,7 @@ import javafx.stage.Stage
 import javafx.scene.Scene
 import javafx.scene.Group
 import javafx.scene.image.Image
+import javafx.scene.image.ImageView
 
 object SlidingPuzzle extends App {
   Application.launch(classOf[SlidingPuzzle], "")
@@ -12,14 +13,16 @@ object SlidingPuzzle extends App {
 
 class SlidingPuzzle extends Application {
 
-  def loadImage(): Image = {
+  lazy val imageView = {
     val inputStream = this.getClass().getResourceAsStream("/2322324186_ca41fba641_o.jpg")
-    new Image(inputStream)
+    val img = new Image(inputStream)
+    new ImageView(img)
   }
 
   override def start(primaryStage: Stage) {
     val group = new Group();
     val scene = new Scene(group);
+    group.getChildren().add(imageView)
     primaryStage.setTitle("Sliding Puzzle")
     primaryStage.setX(100)
     primaryStage.setY(100)
