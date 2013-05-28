bcb676a03dc99cd80727255ac823a760a1c487b7
sebnozzi <sen@192.168.0.12>
2013-05-28 20:57:26 +0200

    Preparing to load image from resources, changed window size

--- src/main/scala/com/sebnozzi/slidingpuzzle/SlidingPuzzle.scala
+++ src/main/scala/com/sebnozzi/slidingpuzzle/SlidingPuzzle.scala
@@ -4,21 +4,27 @@ import javafx.application.Application
 import javafx.stage.Stage
 import javafx.scene.Scene
 import javafx.scene.Group
-
+import javafx.scene.image.Image
 
 object SlidingPuzzle extends App {
   Application.launch(classOf[SlidingPuzzle], "")
 }
 
 class SlidingPuzzle extends Application {
+
+  def loadImage(): Image = {
+    val inputStream = this.getClass().getResourceAsStream("/2322324186_ca41fba641_o.jpg")
+    new Image(inputStream)
+  }
+
   override def start(primaryStage: Stage) {
     val group = new Group();
     val scene = new Scene(group);
     primaryStage.setTitle("Sliding Puzzle")
     primaryStage.setX(100)
     primaryStage.setY(100)
-    primaryStage.setMinWidth(400)
-    primaryStage.setMinHeight(300)
+    primaryStage.setMinWidth(840)
+    primaryStage.setMinHeight(560)
     primaryStage.setResizable(false)
     primaryStage.setFullScreen(false)
     primaryStage.setScene(scene)
