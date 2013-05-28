0219889f3a509d71090bce1ae5b778d23b495c29
sebnozzi <sen@192.168.0.12>
2013-05-28 20:46:18 +0200

    Showing window

--- src/main/scala/com/sebnozzi/slidingpuzzle/SlidingPuzzle.scala
+++ src/main/scala/com/sebnozzi/slidingpuzzle/SlidingPuzzle.scala
@@ -1,5 +1,27 @@
 package com.sebnozzi.slidingpuzzle
 
+import javafx.application.Application
+import javafx.stage.Stage
+import javafx.scene.Scene
+import javafx.scene.Group
+
+
 object SlidingPuzzle extends App {
-  println("Hello, SlidingPuzzle")
+  Application.launch(classOf[SlidingPuzzle], "")
+}
+
+class SlidingPuzzle extends Application {
+  override def start(primaryStage: Stage) {
+    val group = new Group();
+    val scene = new Scene(group);
+    primaryStage.setTitle("Sliding Puzzle")
+    primaryStage.setX(100)
+    primaryStage.setY(100)
+    primaryStage.setMinWidth(400)
+    primaryStage.setMinHeight(300)
+    primaryStage.setResizable(false)
+    primaryStage.setFullScreen(false)
+    primaryStage.setScene(scene)
+    primaryStage.show()
+  }
 }
