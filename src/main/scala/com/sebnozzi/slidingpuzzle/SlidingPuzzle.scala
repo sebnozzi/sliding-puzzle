1efff46022540d8e0523c22c52cdbd8cb11842ed
sebnozzi <sen@192.168.0.12>
2013-06-09 21:39:53 +0200

    Draws grid border (to each slice)

--- src/main/scala/com/sebnozzi/slidingpuzzle/SlidingPuzzle.scala
+++ src/main/scala/com/sebnozzi/slidingpuzzle/SlidingPuzzle.scala
@@ -29,7 +29,7 @@ class SlidingPuzzle extends Application {
     val group = {
       val group = new Group();
       val nodesToAdd = {
-        val slicer = new Slicer(img, xAmount = 4, yAmount = 2)
+        val slicer = new Slicer(img, xAmount = 4, yAmount = 3)
         slicer.slicePositions.map {
           case (x, y) =>
             val xCoord = (x - 1) * slicer.sliceWidth
@@ -37,6 +37,11 @@ class SlidingPuzzle extends Application {
             val slice = slicer.sliceAt(x, y)
             slice.setLayoutX(xCoord)
             slice.setLayoutY(yCoord)
+            val gc = slice.getGraphicsContext2D()
+            gc.beginPath()
+            gc.rect(0, 0, slicer.sliceWidth, slicer.sliceHeight)
+            gc.closePath()
+            gc.stroke()
             slice
         }
       }
