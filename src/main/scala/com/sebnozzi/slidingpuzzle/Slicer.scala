be6b74575c0c3677ff12ca63fd9b83874e7feee3
sebnozzi <sen@192.168.0.12>
2013-06-09 20:57:16 +0200

    slicePositions as val

--- src/main/scala/com/sebnozzi/slidingpuzzle/Slicer.scala
+++ src/main/scala/com/sebnozzi/slidingpuzzle/Slicer.scala
@@ -8,6 +8,13 @@ class Slicer(val img: Image, val xAmount: Int, val yAmount: Int) {
   val sliceWidth = img.getWidth() / xAmount
   val sliceHeight = img.getHeight() / yAmount
 
+  val slicePositions: List[(Int, Int)] = {
+    (for (
+      yPos <- 1 to yAmount;
+      xPos <- 1 to xAmount
+    ) yield (xPos, yPos)).toList
+  }  
+  
   def sliceAt(x: Int, y: Int): Canvas = {
     val canvas = new Canvas(sliceWidth, sliceHeight)
     val grContext = canvas.getGraphicsContext2D()
@@ -22,12 +29,6 @@ class Slicer(val img: Image, val xAmount: Int, val yAmount: Int) {
     canvas
   }
 
-  def slicePositions: List[(Int, Int)] = {
-    (for (
-      yPos <- 1 to yAmount;
-      xPos <- 1 to xAmount
-    ) yield (xPos, yPos)).toList
-  }
 
   def coordinatesOfSliceAt(x: Int, y: Int): (Int, Int) = {
     val coordX = ((x - 1) * sliceWidth).toInt
