88947fa58d0ac4f7be20d841cc9d9f7fff3be681
sebnozzi <sen@192.168.0.12>
2013-06-12 22:42:29 +0200

    named adjacent positions

--- src/main/scala/com/sebnozzi/slidingpuzzle/model/Position.scala
+++ src/main/scala/com/sebnozzi/slidingpuzzle/model/Position.scala
@@ -6,14 +6,28 @@ package object PositionConversions {
 
 case class Position(col: Int, row: Int) {
 
+  lazy val above = Position(col, row - 1)
+  lazy val below = Position(col, row + 1)
+  lazy val left = Position(col - 1, row)
+  lazy val right = Position(col + 1, row)
+
   def adjacentIn(rect: Rect): List[Position] = {
-
-    val above = Position(col, row - 1)
-    val below = Position(col, row + 1)
-    val left = Position(col - 1, row)
-    val right = Position(col + 1, row)
-
     List(above, left, right, below).filter { rect.contains(_) }
+  }
+
+  def aboveIn(rect: Rect): Option[Position] = above.optionallyWithin(rect)
+
+  def belowIn(rect: Rect): Option[Position] = below.optionallyWithin(rect)
+
+  def leftIn(rect: Rect): Option[Position] = left.optionallyWithin(rect)
+
+  def rightIn(rect: Rect): Option[Position] = right.optionallyWithin(rect)
+
+  private def optionallyWithin(rect: Rect): Option[Position] = {
+    if (rect.contains(this))
+      Some(this)
+    else
+      None
   }
 
 }
--- src/test/scala/com/sebnozzi/slidingpuzzle/model/PositionSuite.scala
+++ src/test/scala/com/sebnozzi/slidingpuzzle/model/PositionSuite.scala
@@ -3,7 +3,7 @@ package com.sebnozzi.slidingpuzzle.model
 import org.scalatest.FunSuite
 import org.scalatest.Finders
 
-class PositionSuite extends FunSuite  {
+class PositionSuite extends FunSuite {
 
   test("a Position consists of column and row") {
     val pos1 = Position(col = 1, row = 1)
@@ -13,15 +13,26 @@ class PositionSuite extends FunSuite  {
     assert(pos2.col === 2)
     assert(pos2.row === 3)
   }
-  
+
   test("adjacent positions of 1,1 (top left corner)") {
-    val pos1 = Position(col = 1, row = 1)
-    assert(pos1.adjacentIn(Rect(Position(1,1), Position(4,3))) === List(Position(2,1), Position(1,2)))
-  }  
-  
+    val pos = Position(col = 1, row = 1)
+    val rect = Rect(Position(1, 1), Position(4, 3))
+    assert(pos.adjacentIn(rect) === List(Position(2, 1), Position(1, 2)))
+  }
+
   test("adjacent positions of 4,3 (bottom right corner)") {
-    val pos1 = Position(col = 4, row = 3)
-    assert(pos1.adjacentIn(Rect(Position(1,1), Position(4,3))) === List(Position(4,2), Position(3,3)))
-  }    
-  
+    val pos = Position(col = 4, row = 3)
+    val rect = Rect(Position(1, 1), Position(4, 3))
+    assert(pos.adjacentIn(rect) === List(Position(4, 2), Position(3, 3)))
+  }
+
+  test("named adjacent positions of 1,1") {
+    val pos = Position(col = 1, row = 1)
+    val rect = Rect(Position(1, 1), Position(4, 3))
+    assert(pos.aboveIn(rect) === None)
+    assert(pos.belowIn(rect) === Some(Position(1, 2)))
+    assert(pos.leftIn(rect) === None)
+    assert(pos.rightIn(rect) === Some(Position(2, 1)))
+  }
+
 }
