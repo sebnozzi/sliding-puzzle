568e56df7d484f20c2f951bf9353bd3b1dcc522c
sebnozzi <sen@192.168.0.12>
2013-06-14 00:26:58 +0200

    Rudimentary, non-working, select-box for sizes

--- src/main/scala/com/sebnozzi/slidingpuzzle/ui/javafx/ControlPanel.scala
+++ src/main/scala/com/sebnozzi/slidingpuzzle/ui/javafx/ControlPanel.scala
@@ -8,32 +8,48 @@ import javafx.geometry.Pos
 import javafx.geometry.Insets
 import javafx.scene.control.ToolBar
 import javafx.scene.control.Label
+import javafx.scene.control.ChoiceBox
+import javafx.collections.FXCollections
+import javafx.beans.value.ChangeListener
+import javafx.beans.value.ObservableValue
 
 class ControlPanel extends ToolBar {
 
   private val shuffleButton = new Button("Shuffle")
   private val resetButton = new Button("Reset")
   private val movesLabel = new Label(movesMsg(0))
+  private val sizeSelector = new ChoiceBox[String]()
 
   setup()
 
-  def onShufflePressed(callback:  => Unit) =
+  def onShufflePressed(callback: => Unit) =
     addButtonHandler(shuffleButton) { callback }
 
-  def onResetPressed(callback:  => Unit) =
+  def onResetPressed(callback: => Unit) =
     addButtonHandler(resetButton) { callback }
 
-  def setMovesCount(count:Int) {
+  def setMovesCount(count: Int) {
     movesLabel.setText(movesMsg(count))
   }
-  
-  private def movesMsg(count:Int) = s"Moves: $count"
-  
+
+  private def movesMsg(count: Int) = s"Moves: $count"
+
   private def setup() {
+    ControlPanel.this.getItems().add(sizeSelector)
     ControlPanel.this.getItems().add(shuffleButton)
     ControlPanel.this.getItems().add(resetButton)
     ControlPanel.this.getItems().add(movesLabel)
+
+    sizeSelector.setItems(FXCollections.observableArrayList[String]("3x3", "4x3"))
+    val selectedIndexProperty = sizeSelector.getSelectionModel().selectedIndexProperty()
+    selectedIndexProperty.addListener(new ChangeListener[Number]() {
+      def changed(ov: ObservableValue[_ <: Number], 
+          oldValue: Number, newValue: Number) {
+        println(s"Selected index changed to: $newValue")
+      }
+    });
   }
+  sizeSelector.getSelectionModel().selectFirst()
 
   private def addButtonHandler(button: Button)(block: => Unit) {
     button.setOnAction(new EventHandler[ActionEvent]() {
