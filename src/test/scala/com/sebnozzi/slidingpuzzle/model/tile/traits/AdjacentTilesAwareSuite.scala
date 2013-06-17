fe13c56b3a3610647c689d50c42d5f31d6302f36
sebnozzi <sen@192.168.0.12>
2013-06-17 20:27:36 +0200

    empty suite classes

---
+++ src/test/scala/com/sebnozzi/slidingpuzzle/model/tile/traits/AdjacentTilesAwareSuite.scala
@@ -0,0 +1,7 @@
+package com.sebnozzi.slidingpuzzle.model.tile.traits
+
+import org.scalatest.FunSuite
+
+class AdjacentTilesAwareSuite extends FunSuite {
+
+}
---
+++ src/test/scala/com/sebnozzi/slidingpuzzle/model/tile/traits/VisibilityAwareSuite.scala
@@ -0,0 +1,7 @@
+package com.sebnozzi.slidingpuzzle.model.tile.traits
+
+import org.scalatest.FunSuite
+
+class VisibilityAwareSuite extends FunSuite {
+
+}
