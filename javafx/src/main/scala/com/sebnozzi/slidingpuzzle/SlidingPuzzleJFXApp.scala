e1658923c1a50d263edbc19d3ea26c1c540727ca
sebnozzi <sen@theAir.local>
2013-10-20 13:37:41 +0200

    Re-wrote FX launching

--- javafx/src/main/scala/com/sebnozzi/slidingpuzzle/SlidingPuzzleJFXApp.scala
+++ javafx/src/main/scala/com/sebnozzi/slidingpuzzle/SlidingPuzzleJFXApp.scala
@@ -1,15 +1,18 @@
 package com.sebnozzi.slidingpuzzle
 
+import _root_.javafx.stage.Stage
 import _root_.javafx.application.Application
-import com.sebnozzi.slidingpuzzle.ui.javafx.JFXAppController
-import javafx.stage.Stage
 
+import com.sebnozzi.slidingpuzzle.ui.javafx.JFXAppController
+
+object SlidingPuzzleApp extends App {
+  import Application._
+  launch(classOf[SlidingPuzzleJFXApp], args:_*)
+}
 
 class SlidingPuzzleJFXApp extends Application {
-
   override def start(mainWindow: Stage) {
     val appController = new JFXAppController(mainWindow)
     appController.start()
   }
-
 }
