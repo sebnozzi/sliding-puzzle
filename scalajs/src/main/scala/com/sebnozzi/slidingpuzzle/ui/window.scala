25373a2acf160ddba94f3883c0c32ddfe089c42b
sebnozzi <sen@192.168.0.17>
2013-12-20 15:53:11 +0100

    Using tile-ids for referencing tiles

--- scalajs/src/main/scala/com/sebnozzi/slidingpuzzle/ui/window.scala
+++ scalajs/src/main/scala/com/sebnozzi/slidingpuzzle/ui/window.scala
@@ -3,11 +3,10 @@ package com.sebnozzi.slidingpuzzle.ui
 import scala.scalajs.js
 
 trait JsUIController extends js.Object {
-  type JsTileType = js.Dynamic
-  def getTiles(): Array[JsTileType]
-  def makeTileVisible(jsTile: JsTileType, animate: Boolean)
-  def makeTileHidden(jsTile: JsTileType)
-  def moveTileTo(jsTile: JsTileType, col: Int, row: Int, animate: Boolean)
+  def getTileIds(): Array[String]
+  def makeTileVisible(tileId: String, animate: Boolean)
+  def makeTileHidden(tileId: String)
+  def moveTileTo(tileId: String, col: Int, row: Int, animate: Boolean)
   def onSizeChanged()
   def setMovesCount(newCount:Int)
 }
