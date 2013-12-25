7db7958095b29e1276e7c818b1722b3b5ab5b989
Sebastian Nozzi <sebnozzi@gmail.com>
2013-12-25 01:17:59 +0100

    Modified JS call to prevent name-destruction from Closure compiler

--- scalajs/startup.js
+++ scalajs/startup.js
@@ -1,25 +1,34 @@
-$(document).ready(function() {
+
+$(document)["ready"](function() {
     
-  window.jsUIController = makeJsUIController();
-  var jsUIController = window.jsUIController;
+  window["jsUIController"] = makeJsUIController();
+  var jsUIController = window["jsUIController"];
   
-  $("#sizeSelector").on("change", function() {
-    jsUIController.sizeChanged();
+  $("#sizeSelector")["on"]("change", function() {
+    jsUIController["sizeChanged"]();
   });
 
-  $("#shuffleButton").on("click", function() {
-    jsUIController.shuffleClicked();
+  $("#shuffleButton")["on"]("click", function() {
+    jsUIController["shuffleClicked"]();
   });
 
-  $("#resetButton").on("click", function() {
-    jsUIController.resetClicked();
+  $("#resetButton")["on"]("click", function() {
+    jsUIController["resetClicked"]();
   });
-
-  jsUIController.imageLoaded();
-
-  jsUIController.setupGrid(3,2);
+  
+  var imgSrc = $("#imgAnchor")["attr"]("href");
+  var tmpImg = new Image() ;
+  tmpImg["src"] = imgSrc;
+  tmpImg["onload"] = function() {
+    $("#hiddenContainer")["append"](tmpImg);
+    $(tmpImg)["attr"]("id", "srcImg");
     
-  ScalaJS.modules.com_sebnozzi_slidingpuzzle_SlidingPuzzleMain().main();
+    jsUIController["imageLoaded"]();
+    jsUIController["setupGrid"](3,2);
+      
+    ScalaJS.modules.com_sebnozzi_slidingpuzzle_SlidingPuzzleMain().main();
+    jsUIController["shuffleClicked"]();
+  } ;
   
 });
 
--- scalajs/ui.js
+++ scalajs/ui.js
@@ -16,134 +16,134 @@ function makeJsUIController() {
   var sizeCallback = function(newCols, newRows){};
   
   return {
-    imageLoaded: function() {
+    "imageLoaded": function() {
       var srcImg = $("#srcImg")[0];
-      imgWidth = srcImg.width;
-      imgHeight = srcImg.height;
+      imgWidth = srcImg["width"];
+      imgHeight = srcImg["height"];
     },
-    setupGrid: function(cols, rows) {
+    "setupGrid": function(cols, rows) {
       
-      console.log("Setting up grid: ", cols, "x", rows);
+      //console["log"]("Setting up grid: ", cols, "x", rows);
       
-      $("#puzzle").empty();
+      $("#puzzle")["empty"]();
       tiles = [];
       currentSize = {"cols": cols, "rows": rows};
     
-      tileWidth = Math.floor(imgWidth / cols);
-      tileHeight = Math.floor(imgHeight / rows);
+      tileWidth = Math["floor"](imgWidth / cols);
+      tileHeight = Math["floor"](imgHeight / rows);
       
-      for(row = 0; row < rows; row++) {
-        for(col = 0; col < cols; col++) {
+      for(var row = 0; row < rows; row++) {
+        for(var col = 0; col < cols; col++) {
           var id = "canvas_"+col+"_"+row;
           var tile = $('<canvas id="'+id+'" data-col="'+col+'" data-row="'+row+
             '" class="absolute" width="'+tileWidth+'" height="'+tileHeight+'"/>');
     
-          $("#puzzle").append(tile);
-          tile = tile.get(0);
+          $("#puzzle")["append"](tile);
+          tile = tile["get"](0);
     
-          var img = $("#srcImg").get(0);
-          var ctx = tile.getContext("2d");
+          var img = $("#srcImg")["get"](0);
+          var ctx = tile["getContext"]("2d");
           var sx = col * tileWidth;
           var sy = row * tileHeight;
-          ctx.drawImage(img, sx, sy, tileWidth, tileHeight, 0, 0, tileWidth, tileHeight);
-          ctx.strokeRect(0, 0, tileWidth, tileHeight);
+          ctx["drawImage"](img, sx, sy, tileWidth, tileHeight, 0, 0, tileWidth, tileHeight);
+          ctx["strokeRect"](0, 0, tileWidth, tileHeight);
           
-          $(tile).css("left", sx);
-          $(tile).css("top", sy);
+          $(tile)["css"]("left", sx);
+          $(tile)["css"]("top", sy);
           
-          tiles.push(tile);
+          tiles["push"](tile);
         }
       }
     
       var _thisClosure = this;
-      $(tiles).click(function() {
-        var col = parseInt($(this).attr("data-col"));
-        var row = parseInt($(this).attr("data-row"));
-        var tileId = $(this).attr("id");
-        _thisClosure.tileClicked(tileId);
+      $(tiles)["click"](function() {
+        var col = parseInt($(this)["attr"]("data-col"));
+        var row = parseInt($(this)["attr"]("data-row"));
+        var tileId = $(this)["attr"]("id");
+        _thisClosure["tileClicked"](tileId);
       });
     
     },
-    shuffleClicked: function() {
+    "shuffleClicked": function() {
       shuffleCallback();
     },
-    resetClicked: function() {
+    "resetClicked": function() {
       resetCallback();
     },
-    sizeChanged: function() {
-      var size = this.getSelectedSize();
+    "sizeChanged": function() {
+      var size = this["getSelectedSize"]();
       sizeCallback(size.cols, size.rows);
     },
-    tileClicked: function(tileId) {
+    "tileClicked": function(tileId) {
       //console.log(tileCallback);
       tileCallback(tileId);
     },    
-    findTile: function(id) {
-      for(i = 0; i < tiles.length; i++) {
-        if($(tiles[i]).attr("id") == id)
+    "findTile": function(id) {
+      for(var i = 0; i < tiles["length"]; i++) {
+        if($(tiles[i])["attr"]("id") == id)
           return tiles[i];
       }
       return undefined;
     },    
-    setMovesCount: function(newValue) {
-      $("#movesCount").html(newValue);
+    "setMovesCount": function(newValue) {
+      $("#movesCount")["html"](newValue);
     },
-    getTileIds: function() {
+    "getTileIds": function() {
       var ids = [];
-      for(i=0; i<tiles.length; i++) {
-        ids.push($(tiles[i]).attr("id"));
+      for(var i=0; i < tiles["length"]; i++) {
+        ids["push"]($(tiles[i])["attr"]("id"));
       }
       return ids;
     },
-    makeTileVisible: function(tileId, animate) {
-      var tile = this.findTile(tileId);
+    "makeTileVisible": function(tileId, animate) {
+      var tile = this["findTile"](tileId);
       if(animate) {
-        $(tile).show(400);
+        $(tile)["show"](400);
       } else {
-        $(tile).show();
+        $(tile)["show"]();
       }
     },
-    makeTileHidden: function(tileId) {
-      var tile = this.findTile(tileId);
-      $(tile).hide();
+    "makeTileHidden": function(tileId) {
+      var tile = this["findTile"](tileId);
+      $(tile)["hide"]();
     },
-    moveTileTo: function(tileId, targetCol, targetRow, animate) {
-      var tile = this.findTile(tileId);
+    "moveTileTo": function(tileId, targetCol, targetRow, animate) {
+      var tile = this["findTile"](tileId);
       //console.log("Moving tile: ", tile, " to: ", col, row);
       var left = tileWidth * (targetCol);
       var top = tileHeight * (targetRow);
       if(animate) {
-        $(tile).animate({
-          left: "" + left,
-          top: "" + top,
+        $(tile)["animate"]({
+          "left": "" + left,
+          "top": "" + top,
         }, 100);
       } else {
-        $(tile).css("top", top); 
-        $(tile).css("left", left); 
+        $(tile)["css"]("top", top); 
+        $(tile)["css"]("left", left); 
       }
     },
-    getSelectedSize: function() {
-      var commaSeparatedNumbers = $("#sizeSelector").val();
-      var parts = commaSeparatedNumbers.split(",");
+    "getSelectedSize": function() {
+      var commaSeparatedNumbers = $("#sizeSelector")["val"]();
+      var parts = commaSeparatedNumbers["split"](",");
       var cols = parseInt(parts[0]);
       var rows = parseInt(parts[1]);
       return {"cols": cols, "rows": rows};
     },
-    setSelectedSize: function(cols, rows) {
+    "setSelectedSize": function(cols, rows) {
       var targetVal = cols + "," + rows;
-      console.log("Selecting size value: " + targetVal);
-      $("#sizeSelector").val(targetVal);
+      //console.log("Selecting size value: " + targetVal);
+      $("#sizeSelector")["val"](targetVal);
     },
-    onTileClicked: function(callback) {
+    "onTileClicked": function(callback) {
       tileCallback = callback;
     },    
-    onSizeChanged: function(callback) {
+    "onSizeChanged": function(callback) {
       sizeCallback = callback;
     },    
-    onShufflePressed: function(callback) {
+    "onShufflePressed": function(callback) {
       shuffleCallback = callback;
     },    
-    onResetPressed: function(callback) {
+    "onResetPressed": function(callback) {
       resetCallback = callback;
     }
   };
