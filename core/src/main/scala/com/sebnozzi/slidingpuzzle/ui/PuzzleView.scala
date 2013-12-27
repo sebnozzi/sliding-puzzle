652aa5cd85a848f835fcbd5c59655c93846f5ce4
Sebastian Nozzi <sebnozzi@gmail.com>
2013-12-27 20:54:17 +0100

    Small trait change

--- core/src/main/scala/com/sebnozzi/slidingpuzzle/ui/PuzzleView.scala
+++ core/src/main/scala/com/sebnozzi/slidingpuzzle/ui/PuzzleView.scala
@@ -3,9 +3,6 @@ package com.sebnozzi.slidingpuzzle.ui
 import com.sebnozzi.slidingpuzzle.model.structs.GridSize
 
 trait PuzzleView {
-
   def requestFocus() {}
-
-  val tileViews: List[TileView]
-
+  def tileViews: List[TileView]
 }
