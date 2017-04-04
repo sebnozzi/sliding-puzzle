f51f4839fc3716e515f4ea76a891103f248f7538
Sebastian Nozzi <sebnozzi@gmail.com>
2017-04-04 09:02:30 +0200

    Version change of artifact

--- project/SlidingPuzzleBuild.scala
+++ project/SlidingPuzzleBuild.scala
@@ -17,7 +17,7 @@ object SlidingPuzzleBuild extends Build {
           "-feature",
           "-encoding", "utf8"
       ),
-      version := "0.1-SNAPSHOT"
+      version := "0.2-SNAPSHOT"
   )
 
   val javafxSettings: Seq[Setting[_]] = Seq(
